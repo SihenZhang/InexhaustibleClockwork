540c96006b4cc7eb6b9430f060fb11eaef263b65
SihenZhang <1334125013@qq.com>
2017-02-20 22:18:02 +0800

    Add EnderDiamond

--- CS2_Creator/mod.js
+++ CS2_Creator/mod.js
@@ -7,6 +7,7 @@ mod.addItem("iaComplex.js", "normal");
 mod.addItem("tcsComplex.js", "normal");
 mod.addItem("ingotBloodedCobalt.js", "normal");
 mod.addItem("Sunnarium.js", "normal");
+mod.addItem("enderDiamond.js", "normal");
 mod.addItem("AluminumPlate.js", "normal");
 mod.addItem("BronzePlate.js", "normal");
 mod.addItem("ConstantanPlate.js", "normal");
