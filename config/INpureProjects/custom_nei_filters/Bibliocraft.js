a25e843355af79021012690953c183c79b9698ee
SihenZhang <1334125013@qq.com>
2017-04-02 15:21:52 +0800

    Hide Bibliocraft Addons in NEI

--- config/INpureProjects/custom_nei_filters/Bibliocraft.js
+++ config/INpureProjects/custom_nei_filters/Bibliocraft.js
@@ -1,3 +1,6 @@
 if (FML.isModLoaded("BiblioCraft") && Bibliocraft_enabled) {
     NEI.override("BiblioCraft:*", [0]);
+	NEI.override("BiblioWoodsBoP:*", [0]);
+	NEI.override("BiblioWoodsForestry:*", [0]);
+	NEI.override("BiblioWoodsNatura:*", [0]);
 }
